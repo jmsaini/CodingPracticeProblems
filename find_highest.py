a69461199ec73e8d49a362469fc7c1e80aa9de64
Jasmine <saini.jasmine@hotmail.com>
2021-09-14 16:21:49 -0400

    changed description

--- find_highest.py
+++ find_highest.py
@@ -10,7 +10,8 @@ def find_highest(cart: Dict) -> int:
 
     :param cart: is a dictionary where the key is a unique number given to a
     particular item and the value is the number of times it occurs in the cart
-    :return: the index of the highest value
+    :return: the unique number of the item that has the highest number of occurrence in the cart, 
+    return the first highest occurrence whenever there is a tie 
     >>> find_highest({0: 4, 1: 0, 2: 16, 3: 10, 4: 3})
     2
     >>> find_highest({0: 4, 1: 0, 2: 9, 3: 15, 4: 15, 5: 8, 6: 17, 7: 17})
