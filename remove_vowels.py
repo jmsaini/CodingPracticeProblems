891273c021063ec4674234c55b47fa1dc4e29622
Jasmine <saini.jasmine@hotmail.com>
2021-09-25 10:26:20 -0400

    upload remove_vowels

---
+++ remove_vowels.py
@@ -0,0 +1,20 @@
+def remove_vowels(word: str) -> str:
+    """
+    Remove the vowels from the word and return the word after cleaning
+    :param word: the word contains vowels that need to be removed
+    :return: the word after removing all the vowels
+    >>> remove_vowels('JASmine')
+    'JSmn'
+    >>> remove_vowels('HAllOWorld')
+    'HllWrld'
+    """
+    new_word = ''
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+    for char in word:
+        if char not in vowels:
+            new_word += char
+    return new_word
+
+
+
